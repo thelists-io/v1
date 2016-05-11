45cfe2ab387e1c243f8a4fdae3cde0315842fe69
MiniNaim <mininaim@gmail.com>
2016-05-11 19:26:42 +0100

    update page 404

--- application/views/page404.php
+++ application/views/page404.php
@@ -8,12 +8,12 @@
     </div>
 </section>
 
-<section class="page404" role="404">
+<section class="page404" role="page404">
     <div class="container">
         <div class="row">
             <div class="col-xs-12 col-md-12">
-              <h1>أوووبس</h1>
-              <p>الصفحة التي تبحث عنها فير موجودة.</p>
+              <h1>أوووبس!</h1>
+              <p>الصفحة التي تبحث عنها غير موجودة.</p>
             </div>
         </div>
     </div>
