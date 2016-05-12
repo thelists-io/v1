9699484c5463b646c665fbc1b359ac00237aca11
MiniNaim <mininaim@gmail.com>
2016-05-12 18:47:39 +0100

    improve and clean contact section

--- application/views/contact_us.php
+++ application/views/contact_us.php
@@ -2,60 +2,58 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12 col-md-12">
-                <h2>تواصل معنا</h2>
+                <h1>تواصل معنا</h1>
             </div>
         </div>
     </div>
 </section>
 
-<section class="contact" role="contact">
+<section class="page" role="page">
     <div class="container">
         <div class="row">
-            <div class="col-xs-12 col-md-4">
+            <div class="col-xs-12 col-md-3">
                 <div class="sidebar">
-                    <div class="widget-address" data-spy="affix" id="nav">
-                        <h3>get in touch.</h3>
+                    <div class="block" data-spy="affix" id="nav">
+                        <h4>تواصل معنا</h4>
                         <address>
-                            +966 56 209 8080
+                            +966562098080
                             <br>
-                            <a href="mailto:hello@ar-checklist.com" title="Email Us!">hello@ar-checklist.com</a>
-                        </address>
-                        <h3 class="mt6">come visit us.</h3>
-                        <address>
-                            King Fahd Road, Ghirnatah, Dammam
-                            <br> Baghlaf tower, 3rd Floor, Office 10
-                            <br>
+                            +966562098080
+                            <br><br>
+                            <a href="mailto:hello@thelists.io" title="تواصل معنا" target="_blank">
+                                <i class="fa fa-envelope"></i> hello@thelists.io
+                            </a>
                         </address>
                     </div>
                 </div>
             </div>
-            <div class="col-xs-12 col-md-8 helper-xs-mt2">
-                <h3>feel free to reach out.</h3>
-                <div id="success_message" class="alert alert-success hide">
+            <div class="col-xs-12 col-md-9">
 
+                <div id="success_message" class="alert alert-success hide">
                 </div>
+
                 <form action="" id="contact-form" class="form-horizontal" role="form" method="post">
                     <div class="form-group" id="name-field">
                         <div class="col-md-12">
-                            <input type="text" class="form-control" id="form-name" name="name" placeholder="Your Name" >
-                            <span class="errorMessage" id="error_message_name" style="color:red;"></span>
+                            <input type="text" class="form-control" id="form-name" name="name" placeholder="اسمك">
+                            <span class="errorMessage" id="error_message_name"></span>
                         </div>
 
                     </div>
                     <div class="form-group" id="email-field">
                         <div class="col-md-12">
-                            <input type="email" class="form-control" id="form-email" name="email" placeholder="Your Email Address" >
-                            <span class="errorMessage" id="error_message_email" style="color:red;"></span>
+                            <input type="email" class="form-control" id="form-email" name="email" placeholder="البريد الإلكتروني">
+                            <span class="errorMessage" id="error_message_email"></span>
                         </div>
                     </div>
                     <div class="form-group" id="message-field">
                         <div class="col-md-12">
-                            <textarea class="form-control" rows="8" id="form-message" name="message" placeholder="Message" ></textarea>
-                            <span class="errorMessage" id="error_message_message" style="color:red;"></span>
+                            <textarea class="form-control" rows="8" id="form-message" name="message" placeholder="رسالتك"></textarea>
+                            <span class="errorMessage" id="error_message_message"></span>
                         </div>
                     </div>
-                    <button type="submit" class="btn btn-green" title="drop us a line">
-                        send now
+                    <button type="submit" class="btn btn-green" title="راسلنا الان">
+                        راسلنا الان
                     </button>
 
                 </form>
